de35b86c3ba30a6f445818cb7687d67f88a96c76
Marcelo Bastos <marcelonunesbastos@hotmail.com>
2025-02-13 21:42:16 -0300

    ultimas correções no PlayersUtil

--- src/ReplicatedStorage/PlayersUtil.lua
+++ src/ReplicatedStorage/PlayersUtil.lua
@@ -1,12 +1,15 @@
 local PlayersUtil = {}
 
 function PlayersUtil.contarAmigosNoServidor(jogador:Player)
-	return #game.Players:GetFriendsAsync(jogador.UserId) -- Retorna o número de jogadores
+	if PlayersUtil.contarJogadoresNoServidor() >1 then
+		return #game.Players:GetFriendsAsync(jogador.UserId) -- Retorna o número de jogadores	
+	end
+	return 0
 
 end
 
-function contarJogadoresNoServidor()
-    return #game.Players:GetPlayers() -- Retorna o número de jogadores
+function PlayersUtil.contarJogadoresNoServidor()
+    return #game.Players:GetPlayers()
 end
 
 
